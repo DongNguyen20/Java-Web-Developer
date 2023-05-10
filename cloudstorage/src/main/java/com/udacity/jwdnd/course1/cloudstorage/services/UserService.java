1aa9751d51910129b1b1131817ad5145c3f0bf28
DongNguyen20 <dongtruelove@gmail.com>
2023-05-10 22:24:15 +0700

    create user login

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
@@ -0,0 +1,41 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Service
+public class UserService {
+    private HashService hashService;
+    private UserMapper userMapper;
+
+    public UserService(UserMapper userMapper, HashService hashService) {
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
+
+    public boolean isUsernameAvailable(String username) {
+        return userMapper.getUser(username) == null;
+    }
+
+    private String createSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return Base64.getEncoder().encodeToString(salt);
+    }
+
+    public int createUser(User user) {
+        String encodedSalt = createSalt();
+        String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
+        return userMapper.insert(new User(null, user.getUsername(), encodedSalt, hashedPassword,
+                user.getFirstName(), user.getLastName()));
+    }
+
+    public User getUser(String userName) {
+        return userMapper.getUser(userName);
+    }
+}
