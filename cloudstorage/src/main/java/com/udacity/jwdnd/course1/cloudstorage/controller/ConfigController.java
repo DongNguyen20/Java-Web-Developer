e287f7ddcff8c923f3e67792246b4da24cc83dbe
DongNguyen20 <dongtruelove@gmail.com>
2023-05-11 18:35:03 +0700

    modify handle bad request url

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/ConfigController.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/ConfigController.java
@@ -1,13 +1,19 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ConfigController {
+public class ConfigController implements ErrorController {
 
     @GetMapping(path = "/error")
     public String errorPage() {
-        return "404";
+        return "404.html";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
     }
 }
